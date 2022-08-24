4db4083dabe5c342809aac52b464ca0a3460dbdb
Mohsen <moh.safi.1380@gmail.com>
2022-08-24 18:10:41 +0430

    #

--- src/components/generic/Todo.js
+++ src/components/generic/Todo.js
@@ -11,23 +11,6 @@ export default function Todo () {
         <Button loading={loading}>افزودن</Button>
       </Form>
 
-      {/* <form onSubmit={addTodo}>
-        <input value={task} onChange={e => setTask(e.target.value)} />
-        <button type='submit'>Add todo</button>
-      </form>
-      <ol>
-        {todos.map(todo => (
-          <li key={todo.id}>
-            {todo.id} {todo.title}
-            <span
-              onClick={() => removeTodo(todo.id)}
-              style={{ color: 'red', margin: '10px', cursor: 'pointer' }}
-            >
-              x
-            </span>
-          </li>
-        ))}
-      </ol> */}
     </div>
   )
 }
